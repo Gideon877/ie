f169f90fd8587539c58b8ea72a6ff3fba6410c99
gideon877 <tblar84@gmail.com>
2017-04-26 16:37:27 +0200

    modification and improvement to an existing code

--- event_reminder.js
+++ event_reminder.js
@@ -1,9 +1,4 @@
-
-// Set the date we're counting down to
-
-// Update the count down every 1 second
-
-function myFunction() {
+function myFunction(){
     var myName = document.getElementById('myName').value;
     myName = myName.toLowerCase();
     function nameSearch(input){
@@ -15,7 +10,7 @@ function myFunction() {
     var t = myDate.slice(0,4);
     myDate = myDate.replace(t,'2017');
     var y = 2017 - t;
-
+    check(myDate)
     // document.getElementById('yrs').innerHTML = "S/he is turning " + y + " years old.";
     //console.log("turning " + y);
 
@@ -23,7 +18,9 @@ function myFunction() {
     var template = Handlebars.compile(display.innerHTML);
     var myDisplay = template({n});
     output.innerHTML = myDisplay;
+}
 
+function check(myDate) {
     var countDownDate = new Date(myDate).getTime();
     var x = setInterval(function myCount(){
     // Get todays date and time
@@ -47,46 +44,53 @@ function myFunction() {
         showTm.innerHTML = "EXPIRED";
     }
     myName = "";
-    var k = document.getElementById('k');
-    var template = Handlebars.compile(k.innerHTML);
-    var myK = template({names});
-    document.getElementById('search').innerHTML = myK;
 
 }, 1000)};
+
+var names = [
+    {name:'codex', eventType:'demo', dob:'2017-04-25'},
+    {name:'viwe', eventType:"birthday", dob:'2010-12-20'},
+    {name:'loyiso', eventType:'birthday', dob:'2017-11-14'}
+];
+var k = document.getElementById('k');
+var template = Handlebars.compile(k.innerHTML);
+var myK = template({names});
+document.getElementById('yrs').innerHTML = myK;
+
 function addName(){
     var newName = document.getElementById("newName").value;
     var neweventType = document.getElementById("neweventType").value;
     var newDob = document.getElementById('newDob').value; // var str = "2017-09-20";
     var name1 = {};
+    function checkNames(input) {return input.name == newName;}
+    var checkedNames = names.filter(checkNames);
+    if(checkedNames.length < 1){
 
-    CreateProp("name",newName.toLowerCase());
-    CreateProp("eventType", neweventType.toLowerCase());
-    CreateProp("dob", newDob);
+        CreateProp("name",newName.toLowerCase());
+        CreateProp("eventType", neweventType.toLowerCase());
+        CreateProp("dob", newDob);
 
-    function CreateProp(propertyName, propertyValue){
-        name1[propertyName] = propertyValue;
-      }
+        function CreateProp(propertyName, propertyValue){
+            name1[propertyName] = propertyValue;
+        }
 
-    names.push(name1);
+        names.push(name1);
+    } else {
+        alert("Name Exist")
+    }
+    var k = document.getElementById('k');
+    var template = Handlebars.compile(k.innerHTML);
+    var myK = template({names});
+    document.getElementById('yrs').innerHTML = myK;
+
     newDob.value = "";
     newName.value = "";
     neweventType.value = "";
 
 }
-
-var names = [
-	{name:'codex', eventType:'demo', dob:'2017-04-25'},
-	{name:'viwe', eventType:"birthday", dob:'2010-12-20'},
-	{name:'loyiso', eventType:'birthday', dob:'2017-11-14'}
-];
-function hideShowSearch(){
-    var z = document.getElementById('add');
-
-    if (z.style.display === 'none') {
-        z.style.display = 'block';
-
-    } else {
-        z.style.display = 'none'
-    }
-
+function hideShowSearch(){var z = document.getElementById('add');
+    if (z.style.display === 'none') {z.style.display = 'block';} else {z.style.display = 'none'}
+}
+function hideNames(){var z = document.getElementById('yrs');
+    if (z.style.display === 'none') {z.style.display = 'block';} else {z.style.display = 'none'}
 }
